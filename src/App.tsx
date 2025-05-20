69a9930b1b035f2c7c194c3969986db9aedd7ec9
James Stewart <auminist3@gmail.com>
2025-05-20 12:24:12 +1200

    Changed the layout of the app to include 2 palletlist components on either side of the grid.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import GridLayout from "./components/GridLayout";
 import Navbar from "./components/Navbar";
+import PalletList from "./components/PalletList";
 
 function App() {
     const [selectedCells, setSelectedCells] = useState<number[]>([]);
@@ -10,11 +11,18 @@ function App() {
         <div className="bg-slate-600 w-screen h-screen">
             <Navbar />
 
+            <div className="flex justify-evenly h-[calc(100vh-56px)] overflow-hidden">
+            <PalletList className="bg-slate-900 overflow-y-auto w-60" />
+            
             <GridLayout
                 gridSize={gridSize}
                 selectedCells={selectedCells}
                 setSelectedCells={setSelectedCells}
             />
+
+            <PalletList className="bg-slate-900 overflow-y-auto w-60" />
+            </div>
+
         </div>
     );
 }
