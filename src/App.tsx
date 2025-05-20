c47155d3aac4b649f04cc3681ff4d6469fdaddc0
James Stewart <auminist3@gmail.com>
2025-05-21 11:51:24 +1200

    changed the layout and styling to feature only 1 palletlist and grid component

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,22 @@ function App() {
         <div className="bg-slate-600 w-screen h-screen">
             <Navbar />
 
-            <div className="flex justify-evenly h-[calc(100vh-56px)] overflow-hidden">
-            <PalletList className="bg-slate-900 overflow-y-auto w-60" />
-            
-            <GridLayout
-                gridSize={gridSize}
-                selectedCells={selectedCells}
-                setSelectedCells={setSelectedCells}
-            />
+            <div className="relative h-[calc(100vh-56px)]">
+                
+                <div className="absolute left-0 top-0 bottom-0 w-60 overflow-y-auto bg-slate-900">
+                    <PalletList className="h-full px-2 py-4" />
+                </div>
 
-            <PalletList className="bg-slate-900 overflow-y-auto w-60" />
+                <div className="flex justify-center items-start h-full">
+                    <div className="ml-60">
+                        <GridLayout
+                            gridSize={gridSize}
+                            selectedCells={selectedCells}
+                            setSelectedCells={setSelectedCells}
+                        />
+                    </div>
+                </div>
             </div>
-
         </div>
     );
 }
